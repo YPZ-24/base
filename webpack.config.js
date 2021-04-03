8eb99212689d18f9bcb45e0c336c2d6d91d1961b
YPZ-24 <aylinypz@gmail.com>
2021-04-02 21:11:12 -0600

    basico webpack babel nodemon

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+//  webpack.config.js
+
+const path = require('path');
+const NodemonPlugin = require('nodemon-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  module: {
+      rules: [
+          {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env'],
+                plugins: ['@babel/plugin-transform-runtime']
+              }
+            }
+          }
+      ]
+  },
+  plugins:[
+    new NodemonPlugin()
+  ]
+};
